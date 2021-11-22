447cacd847e7961c74f96c6894fde32a6687da38
zinstack625 <klimanov625@outlook.com>
2021-11-22 11:50:53 +0300

    [lab-1]: lab done

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use ndarray::*;
+use simplex_method::Table;
+
+fn main() {
+    let constr_coeff =
+        array![[4f64, 1f64, 1f64],
+         [1f64, 1f64, 0f64],
+         [0f64, 0.5f64, 4f64]];
+    let func_coeff =
+         array![1f64, 5f64, 5f64];
+    let constr_val =
+         array![6f64, 5f64, 5f64];
+    let mut table = Table::new(constr_coeff, constr_val, func_coeff, false);
+    table.optimise().expect("Something's gone wrong");
+    println!("{}", table);
+}
